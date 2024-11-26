7e31642e33c233c6e8ed41e61b2de7011ab86d80
ahkharsha <ahkharsha@gmail.com>
2024-11-27 00:18:46 +0530

    Minor fixes

--- client/PageComponents/DetailPage/DetailSeven.jsx
+++ client/PageComponents/DetailPage/DetailSeven.jsx
@@ -29,7 +29,7 @@ const DetailSeven = ({
               <h6 class="title">Price</h6>
               <textarea
                 name="message"
-                placeholder={`Old Price: ${property?.price} GAS`}
+                placeholder={`Old Price: ${property?.price} XTZ`}
                 onChange={(e) =>
                   setUpdatePropertyPrice({
                     ...updatePropertyPrice,
